74a80154cbd000e54ca3139f3780a9e19bd8c240
marinaluiza <marinaluizav@gmail.com>
2020-02-08 13:13:44 -0300

    CORS

--- src/main/java/com/bridgeback/resource/RepositoriesResource.java
+++ src/main/java/com/bridgeback/resource/RepositoriesResource.java
@@ -6,10 +6,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -18,6 +15,7 @@ import java.util.List;
 @RestController
 public class RepositoriesResource {
 
+    @CrossOrigin
     @RequestMapping(value = "/repositories/{name}", method = RequestMethod.GET)
     public ResponseEntity<List<Repository>> listRepositories(@PathVariable String name) {
         try {
@@ -32,6 +30,7 @@ public class RepositoriesResource {
         }
     }
 
+    @CrossOrigin
     @RequestMapping(value = "/users/{owner}/repos", method = RequestMethod.GET)
     public ResponseEntity<List<Repository>> getUserRepository(@PathVariable String owner) {
         try {
